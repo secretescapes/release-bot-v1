e6d1bee0adf2f37b5305d2367bb728eff3bc84fd
mario595 <mariomartinez595@gmail.com>
2016-10-28 20:03:41 +0100

    update option

--- merge-lock-user-service/handler.py
+++ merge-lock-user-service/handler.py
@@ -1,84 +1,74 @@
 import json
+import boto3
+import logging
 
-def create(event, context):
-    body = {
-        "message": "CREATE",
-        "input": event
-    }
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    };
+_client = boto3.client('dynamodb')
+_dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
 
-    return response
+def update(event, context):
+    try:
+        logger.info("Create invoked with event: %s" % event)
+        (username, githubUsername) = _getParameters(event['body'])
+        item = {
+            "username": username,
+            "githubUsername": githubUsername
+        }
+        table = _getTable('users')
+        response = _insert(item, table)
+        logger.info("Response: %s" %response)
+        if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
+            return {
+                "statusCode": 200
+            }
+        else:
+            return {
+                "statusCode": 500,
+                "errorMsg": "Database error"
+            }
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    except KeyError as key:
+        logger.error("Missing key: %s" % key)
+        return {
+            "statusCode": 500,
+            "errorMsg": "Error processing request"
+        }
+    except Exception as e:
+        logger.error("Exception: %s" % e)
+        return {
+            "statusCode": 500,
+            "errorMsg": "Error processing request"
+        }
+   
 
 def list_all(event, context):
-    body = {
-        "message": "LIST ALL",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    };
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "List all",
         "event": event
     }
-    """
 
 def list(event, context):
-    body = {
-        "message": "LIST",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    };
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
+    
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "List",
         "event": event
     }
-    """
+    
 def delete(event, context):
-    body = {
-        "message": "DELETE",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    };
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Delete",
         "event": event
     }
-    """
+
+def _getTable(table_name):
+    return _dynamodb.Table(table_name)
+
+def _getParameters(body):
+    return (body['username'], body['githubUsername'])
+
+def _insert(item, table):
+    return table.put_item (
+                Item = item
+            )   
