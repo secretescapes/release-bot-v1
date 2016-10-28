dc81f1cf4d62c8aafe164e97c113ab90f1da9688
Mario <mario.martinez@secretescapes.com>
2016-10-28 12:23:37 +0100

    Tidy up

--- slack-merge-lock-service/handler.py
+++ slack-merge-lock-service/handler.py
@@ -14,40 +14,18 @@ import requests
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# def dispatcher(event, context):
-#     logger.info("Event received by dispatcher: %s" % event)
-    # for record in event['Records']:
-    #     try:
-    #         eventItem = record['dynamodb']['NewImage']
-    #         if (eventItem['eventType']['S'].upper() == "LIST_RESPONSE"):
-    #             response_url = eventItem['response_url']['S']
-    #             payload = eventItem['payload']['S']
-    #             logger.info("response url: %s" % response_url)
-    #             logger.info("payload: %s" % payload)
-    #             response_text = "{'text': %s}" % _process_payload(payload)
-    #             logger.info("Response body: %s" % response_text)
-    #             requests.post(response_url, data = response_text)
-    #         else:
-    #             pass
-    #     except KeyError as e:
-    #         logger.error("Unrecognized key %s" % e)
-    
 def merge_lock(event, context):
-    params = event.get('body')
-    response_url = params.get('response_url')
-    text = params.get('text').split()
-    if len(text) == 1 and text[0].lower() == 'list':
-        _sns = boto3.client('sns')
-        sns_response = _sns.publish(
-            TopicArn='arn:aws:sns:eu-west-1:015754386147:listQueueRequest',
-            Message='{"response_url": "%s", "requester":"SLACK_MERGE_LOCK_SERVICE"}' % response_url,
-            MessageStructure='string'
-        )
-        logger.info("Sns publish: %s" % sns_response)
-    else:
-        return {
-            "text": "unrecognized command"
-        }
+    try:
+        params = event.get('body')
+        response_url = params.get('response_url')
+        text = params.get('text').split()
+        if len(text) == 1 and text[0].lower() == 'list':
+            _list_request_handler(response_url)
+            return {"text": "I am grabbing the info for you, just hang on a little bit"}
+        else:
+            return {"text": "unrecognized command"}
+    except:
+        return {"text": "Something went really wrong, sorry"}
 
 def dispatcher_list_response(event, context):
     logger.info("List dispatcher invoke with event: %s" % event)
@@ -64,8 +42,16 @@ def dispatcher_list_response(event, context):
             logger.error("Unrecognized key: %s" % e)
 
 
+def _list_request_handler(response_url):
+    _sns = boto3.client('sns')
+    sns_response = _sns.publish(
+        TopicArn='arn:aws:sns:eu-west-1:015754386147:listQueueRequest',
+        Message='{"response_url": "%s", "requester":"SLACK_MERGE_LOCK_SERVICE"}' % response_url,
+        MessageStructure='string'
+    )
+    logger.info("Sns publish: %s" % sns_response)
+
 def _process_payload(obj):
-    # obj = json.loads(payload)
     output = ""
     position = 0
     for item in obj['text']:
